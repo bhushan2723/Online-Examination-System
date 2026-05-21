18a79c3e48cf8ec26b12e81dfc8540a8e6d19f59
bhushan2723 <bhushu2723@gmail.com>
2026-05-21 10:49:42 +0530

    Updated API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://13.234.48.124:5000/api' });
+const API = axios.create({ baseURL: 'http://13.233.109.98:5000/api' });
 
 export const fetchUsers = () => API.get('/users');
 export const createUser = (user) => API.post('/users', user);
